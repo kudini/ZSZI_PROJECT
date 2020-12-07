3a86ac73a67e5500b339c3a34942d7c432ddf343
Mikołaj <mikolajdemi@gmail.com>
2020-12-07 23:32:05 +0100

    Siema2

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
-
-print("hello world")
+print("Hello world")
 print(5+8)
 print(6*6)
 print('Wimir jest the best')
 print("Kocham dr Ciesielkę")
-print("mikołaj pobieraj to szybciej")
+print("Mikołaj pobrał to szybko")
 print("Siema")
